38a538d7fadafd558733fab383c9d1a4df6f5ce5
Andrey <MrAbhorrent@yandex.ru>
2024-09-08 00:00:26 +0500

    Next step variation

---
+++ src/com/education/exam2/Controller/MenuController.java
@@ -0,0 +1,54 @@
+package com.education.exam2.Controller;
+
+import com.education.exam2.View.View;
+
+import java.util.List;
+
+public class MenuController {
+    private final RegistryService registryService;
+    private final View view;
+
+    public MenuController(RegistryService registryService, View view) {
+        this.registryService = registryService;
+        this.view = view;
+    }
+
+    public void showMenu() {
+        while (true) {
+            view.displayMenu();
+            int choice = view.getUserChoice();
+
+            switch (choice) {
+                case 1:
+                    String type = view.getAnimalType();
+                    String name = view.getAnimalName();
+                    registryService.addAnimal(type, name);
+                    view.showAnimalAdded(name);
+                    break;
+                case 2:
+                    name = view.getAnimalName();
+                    List<String> commands = registryService.getCommands(name);
+                    if (commands != null) {
+                        view.showCommands(name, commands);
+                    } else {
+                        view.showAnimalNotFound();
+                    }
+                    break;
+                case 3:
+                    name = view.getAnimalName();
+                    String command = view.getNewCommand();
+                    if (registryService.trainAnimal(name, command)) {
+                        view.showCommandAdded(name, command);
+                    } else {
+                        view.showAnimalNotFound();
+                    }
+                    break;
+                case 4:
+                    view.showExitMessage();
+                    return;
+                default:
+                    view.showInvalidChoice();
+            }
+        }
+    }
+}
--- src/com/education/exam2/View/View.java
+++ src/com/education/exam2/View/View.java
@@ -1,6 +1,18 @@
 package com.education.exam2.View;
 
+import java.util.List;
+
 public interface View {
 
     void displayMenu();
+    int getUserChoice();
+    String getAnimalType();
+    String getAnimalName();
+    String getNewCommand();
+    void showCommands(String name, List<String> commands);
+    void showAnimalAdded(String name);
+    void showAnimalNotFound();
+    void showInvalidChoice();
+    void showExitMessage();
+
 }
